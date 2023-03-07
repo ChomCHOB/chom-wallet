8bdfb71a4ef4cb06bbcfeba09390f07a2b016e71
chaleomwat <39171495+chaleomwat@users.noreply.github.com>
2023-03-07 19:20:47 +0700

    add libs

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,17 @@
+import axios from 'axios'
+import jwt from 'jsonwebtoken'
+import Cookies from 'js-cookie'
+
+
 type ConnectParams = {
   uxMode: 'popup' | 'redirect',
   redirectUrl: string,
 }
 
 type CallOptions = {
-  uxMode?: 'popup' | 'redirect',
-  redirectUrl?: string,
+  ux_mode: 'popup' | 'redirect',
+  signature?: string,
+  redirect_uri?: string,
 }
 
 export class ChomWallet {
@@ -36,41 +42,89 @@ export class ChomWallet {
     this.expiredAt = expiredAt
   }
 
-  private static async  requestLoginUrl(uxMode: 'popup' | 'redirect'): Promise<string> {
+  private static async requestLoginUrl(options: CallOptions): Promise<string> {
     if (!ChomWallet.CLIENT_ID || !ChomWallet.CLIENT_SECRET) {
       throw new Error('Client not initialized')
     }
 
-    // axios.post('//chomwallet-api.chomchob.com', { CLIENT_ID, CLIENT_SECRET, uxMode})
-    const loginUrl = ''
+    const payload: CallOptions = {
+      ux_mode: options.ux_mode
+    }
+
+    if (options.redirect_uri) {
+      payload.redirect_uri = options.redirect_uri
+    }
+
+    const token: string = jwt.sign(payload, ChomWallet.CLIENT_SECRET, {
+      algorithm: 'HS512',
+      noTimestamp: true,
+    })
+
+    if (token) {
+      payload.signature = token
+    }
+
+    const loginUrl: any = await axios(`${ChomWallet.API_URL}/v1/app/login`, {
+      method: 'GET',
+      headers: {
+        "client-id": ChomWallet.CLIENT_ID,
+        Origin: window.location.hostname
+      },
+      params: payload
+    })
+
     return loginUrl
   }
-  static async  connect(options: CallOptions): Promise<ChomWallet> {
+
+  static async connect(options: CallOptions): Promise<ChomWallet> {
     if (!ChomWallet.CLIENT_ID || !ChomWallet.CLIENT_SECRET) {
       throw new Error('Client not initialized')
     }
 
-    if (options.uxMode === 'popup') {
-      const loginUrl = await this.requestLoginUrl(options.uxMode)
+    
+    // if (options.uxMode === 'popup') {
+    //   const loginUrl = await this.requestLoginUrl(options.uxMode)
       
-      // todo: open login in popup and listening access_token
-      const { accessToken, expiredAt } = { accessToken: '', expiredAt: 100000 }
+    //   // todo: open login in popup and listening access_token
+    //   const { accessToken, expiredAt } = { accessToken: '', expiredAt: 100000 }
 
-      // todo: query user profile
-      const { address, accountId } = { address: '', accountId: '' }
+    //   // todo: query user profile
+    //   const { address, accountId } = { address: '', accountId: '' }
 
-      return new ChomWallet(address, accountId, accessToken, expiredAt)
+    //   return new ChomWallet(address, accountId, accessToken, expiredAt)
+    // } else {
+    //   // todo: redirect flow
+    //   return new ChomWallet('', '', '', 0)
+    // }
+
+    // if () {
+    //   return new ChomWallet('', '', '', 0)
+    // } else {
+    //   return new ChomWallet('', '', '', 0)
+    // }
+
+    const accessToken: string = Cookies.get('u_info') || ''
+    if (accessToken) {
+      return new ChomWallet('', '', '', 0)
     } else {
-      // todo: redirect flow
+      const loginUrl = await this.requestLoginUrl(options)
+
+      if (loginUrl && options.ux_mode === 'popup') {
+        window.open(loginUrl, '_blank', 'location=yes,scrollbars=yes,status=yes,width=400,height=400')
+      } else {
+        window.location.href = loginUrl
+      }
       return new ChomWallet('', '', '', 0)
     }
   }
 
-  async signMessage(msg: string, options: CallOptions = {}): Promise<string> {
+  
+
+  async signMessage(msg: string, options: CallOptions): Promise<string> {
     return 'sig'
   }
 
-  async signTypedData(options: CallOptions = {}): Promise<string> {
+  async signTypedData(options: CallOptions): Promise<string> {
     return 'sig'
   }
 
